95fac5f688dfb388c2b81bc977fce92a2d80f897
noobtron <noobtron@gmail.com>
2020-03-23 09:00:13 +0200

    Create hello.cs

---
+++ hello.cs
@@ -0,0 +1,10 @@
+// Hello World!
+namespace HelloWorld
+{
+    class Hello {         
+        static void Main(string[] args)
+        {
+            System.Console.WriteLine("Hello World!");
+        }
+    }
+}
